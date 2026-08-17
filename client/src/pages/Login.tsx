626153ba368fc8ae1305a55c56a17d76750716d3
archie955 <archiesmedley@gmail.com>
2026-08-17 14:24:15 +0100

    add functionality to login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,10 @@
 import { useLogin, useChangeActions } from "../stores/loginStore";
 import RegistrationForm from "../components/RegistrationForm";
-import LoginForm from "../components/LoginForm";
+// import LoginForm from "../components/LoginForm";
 import AuthPage from "../components/AuthPage";
 import FormFooter from "../components/FormFooter";
 import AppLayout from "../layout/AppLayout";
+import { LoginForm } from "@/components/login-form";
 
 const Login = () => {
   const login = useLogin();
