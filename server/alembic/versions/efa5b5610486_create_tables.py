93ead2da9c7b84e4de9dad1b2835ef609ef7bacc
archie955 <archiesmedley@gmail.com>
2026-09-18 16:16:38 +0100

    update alembic migration

--- server/alembic/versions/efa5b5610486_create_tables.py
+++ server/alembic/versions/efa5b5610486_create_tables.py
@@ -125,16 +125,6 @@ def upgrade() -> None:
             nullable=False
         ),
         sa.Column(
-            "attempts",
-            sa.Integer,
-            nullable=True
-        ),
-        sa.Column(
-            "lockout",
-            sa.DateTime(timezone=True),
-            nullable=True
-        ),
-        sa.Column(
             "user_id",
             sa.Integer,
             sa.ForeignKey("user.id", ondelete="CASCADE"),
