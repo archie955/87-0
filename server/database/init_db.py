6fb54d39827b01c5301faca5d8897e72da5e9d78
archie955 <archiesmedley@gmail.com>
2026-09-18 20:36:01 +0100

    fix database seeding by initialising data version if none exists

--- server/database/init_db.py
+++ server/database/init_db.py
@@ -1,6 +1,5 @@
-from exceptions.app_exceptions import DataNotFoundError
 from utils.config import Settings
-from services.helpers import safe_commit_delete
+from services.helpers import safe_commit_delete, safe_commit
 from sqlalchemy import select, delete
 
 from database.database import AsyncSessionLocal
@@ -15,17 +14,21 @@ async def initialise_db(settings: Settings) -> None:
         has_teams = result.scalar_one_or_none() is not None
 
         version = (await db.execute(select(
-            models.DataVersion.version
+            models.DataVersion
         ).limit(1))).scalar_one_or_none()
 
         if not version:
-            raise DataNotFoundError("Database version")
+            version = models.DataVersion(version=1)
+            db.add(version)
+            await safe_commit(db=db, datatype="DB Version")
+            await db.refresh(version)
         
 
         if has_teams and version < settings.data_version:
 
             await db.execute(delete(models.Player))
             await db.execute(delete(models.Team))
+            version.version = settings.data_version
 
             await safe_commit_delete(db=db, datatype="Teams and Players")
         elif has_teams:
